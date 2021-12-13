837bfd14df64df1b0aac1fac483a086ceeac3f12
Eneya <BSC-COM-05-19@lab.cc.ac.mw>
2021-12-13 17:04:04 +0200

    the area program has been written

--- RectangleArea.java
+++ RectangleArea.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class RectangleArea{
   public static void main(String [] args){
+  Scanner input = new Scanner(System.in);
   System.out.println("The area will be shown here");
+  System.out.println("Enter length");
+  double leng = input.nextDouble();
+System.out.println("Enter breadth");
+  double bread = input.nextDouble();
+ Area calcarea = new Area(leng, bread);
   }
+}
+class Area{
+  private double length, breadth;
+  Area(double length, double breadth){
+ leng = length;
+  bread = breadth;
+  }
+ public double returnarea(){
+   double area = length * breadth;
+  return area;
+ }
 }
